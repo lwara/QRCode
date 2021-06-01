550016dc1cacadfc81807f77bb0728390b304f83
lwara <lwaratiwo94@gmail.com>
2021-06-01 22:33:48 +0200

    add disk usage

--- app.py
+++ app.py
@@ -14,27 +14,37 @@ input_HDD_toa = "got it "
 qr = qrcode.QRCode(version=1, box_size=10, border=5)
 #qr.add_data(input_RAM_used)
 
-
-
-
-def pprint_ntuple(nt):
-    for name in nt._fields:
-        value = getattr(nt, name)
+def get_RAM_details(ram):
+    qr.add_data("MEMORY\n")
+    for name in ram._fields:
+        value = getattr(ram, name)
         if name != 'percent':
             value = bytes2human(value)
-        print('%-10s : %7s' % (name.capitalize(), value))
-        qr.add_data('%-10s : %7s' % (name.capitalize(), value))
-        qr.make(fit=True)
-        img = qr.make_image(fill='black', back_color='white')
-        img.save('qrcode001.png')
+        add_QR_data('%-10s : %7s' % (name.capitalize(), value))
 
+def get_disk_usage(disk_space):
+    qr.add_data("DISK SPACE\n")
+    for name in disk_space._fields:
+        value = getattr(disk_space, name)
+        if name != 'percent':
+            value = bytes2human(value)
+        add_QR_data(value)
+
+
+
+
+
+
+def add_QR_data(input_data):
+    qr.add_data(input_data)
+    qr.make(fit=True)
+    img = qr.make_image(fill='black', back_color='white')
+    img.save('qrcode001.png')
 
 def main():
     print('MEMORY\n------')
-    pprint_ntuple(psutil.virtual_memory())
-    #print('\nSWAP\n----')
-    #pprint_ntuple(psutil.swap_memory())
-
+    get_RAM_details(psutil.virtual_memory())
+    get_disk_usage(psutil.disk_usage('/'))
 
 if __name__ == '__main__':
     main()
