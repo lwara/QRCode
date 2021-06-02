02e2dbbf0dfe840e43b6259badda2942152d076f
lwara <lwaratiwo94@gmail.com>
2021-06-02 10:47:08 +0200

    wrap it with flask python

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
+import flask
 import psutil as psutil
 import qrcode
 import psutil
 from psutil._common import bytes2human
-
+from flask import Flask, render_template
 import psutil._common
+
+app = Flask(__name__)
+
 
 #sample data
 input_RAM_used = "RAM % Space Used :", psutil.virtual_memory()
-input_HDD_toa = "got it "
 
 #Create QR Code Instance
-
 qr = qrcode.QRCode(version=1, box_size=10, border=5)
-#qr.add_data(input_RAM_used)
 
 def get_RAM_details(ram):
     qr.add_data("MEMORY\n")
@@ -20,7 +21,7 @@ def get_RAM_details(ram):
         value = getattr(ram, name)
         if name != 'percent':
             value = bytes2human(value)
-        add_QR_data('%-10s : %7s' % (name.capitalize(), value))
+        add_QR_data('%-10s : %7s' % (name.capitalize(), value)+'\n')
 
 def get_disk_usage(disk_space):
     qr.add_data("DISK SPACE\n")
@@ -28,7 +29,7 @@ def get_disk_usage(disk_space):
         value = getattr(disk_space, name)
         if name != 'percent':
             value = bytes2human(value)
-        add_QR_data(value)
+        add_QR_data('%-10s : %7s' % (name.capitalize(), value)+'\n')
 
 
 
@@ -41,10 +42,21 @@ def add_QR_data(input_data):
     img = qr.make_image(fill='black', back_color='white')
     img.save('qrcode001.png')
 
+@app.route('/getQrImage')
+def get_image():
+    image_name = 'qrcode001.png'
+    #Simage_name.Seek(0)
+    return flask.send_file(image_name, mimetype='png')
+
+
 def main():
-    print('MEMORY\n------')
     get_RAM_details(psutil.virtual_memory())
     get_disk_usage(psutil.disk_usage('/'))
+    get_image()
+
+
 
 if __name__ == '__main__':
-    main()
+    app.run(host='0.0.0.0', debug=True)
+    #main()
+
